aa9c11881f0cc66d8dc086b0d52b41d4e97792e1
Andres04Escobedo <andres404escobedo@gmail.com>
2024-10-26 06:38:07 -0500

    Delete Python Jardines directory

--- Python Jardines/Actividad5.py
+++
@@ -1,9 +0,0 @@
-a = 40
-b = 30
-
-if b > a:
-        print ("b es mayor que a")
-elif b == a:
-        print ("a y b son iguales")
-else:
-        print ("a es mayor que b")
--- Python Jardines/Prueba.py
+++
@@ -1,9 +0,0 @@
-signo_numero = input("Escribe un numero: ")
-numero = float(signo_numero)
-
-if numero == 0:
-    print ("El numero es cero")
-elif numero < 0:
-    print ("El numero es negativo")
-else:
-    print ("El numero es positivo")
